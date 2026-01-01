9061ea8d1e600510cbd69dd337ceebdf3bbfb2b6
OneNet <79188819+onenet786@users.noreply.github.com>
2026-01-02 01:17:04 +0500

    123

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.servenow"
+        applicationId = "com.onenetsolutions.servenow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -43,7 +43,7 @@ android {
     signingConfigs {
         create("release") {
             if (hasReleaseKeystore) {
-                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storeFile = rootProject.file(keystoreProperties.getProperty("storeFile"))
                 storePassword = keystoreProperties.getProperty("storePassword")
                 keyAlias = keystoreProperties.getProperty("keyAlias")
                 keyPassword = keystoreProperties.getProperty("keyPassword")
